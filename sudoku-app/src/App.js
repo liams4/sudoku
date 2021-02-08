246513e749cfc69fc84c5ee327bf0525421b20da
Liam Sexton <liams4@uw.edu>
2021-02-07 17:39:16 -0800

    Add basic routing between components

--- sudoku-app/src/App.js
+++ sudoku-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Home, About} from './Components.js';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+          <div className='tabs'>
+              <Link to='/' className='tab'>Home</Link>
+              <Link to='/about' className='tab'>About</Link>
+          </div>
+        <Switch>
+        <Route exact path='/'>
+            <Home />
+          </Route>
+        <Route exact path='/about'>
+            <About />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ sudoku-app/src/Components.js
@@ -0,0 +1,17 @@
+export function Home() {
+    return (
+        <Board/>
+    )
+}
+
+export function About() {
+    return (
+        <h1>Built with React</h1>
+    )
+}
+
+export function Board() {
+    return (
+        <h1>board</h1>
+    )
+}
